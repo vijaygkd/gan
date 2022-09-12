ed5cc1768df540bfc15a85d12d6307fcc5510a2b
Vijay Gaikwad <vijay@loftium.com>
2022-09-12 16:12:40 -0700

    update loss function

--- src/model.py
+++ src/model.py
@@ -5,17 +5,18 @@ import tensorflow as tf
 class GAN(tf.keras.Model):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.generator = self.init_generator()
+        self.noise_dim = 28*28     # noise input dimension
+        self.generator = self.init_generator(self.noise_dim)
         self.discriminator = self.init_discriminator()
-        self.latent_dim = 28*28
+
         print("Hello 1")
 
     @staticmethod
-    def init_generator():
-        input = layers.Input(shape=(28*28, ))
+    def init_generator(noise_dim):
+        input = layers.Input(shape=(noise_dim, ))
         h = layers.Dense(256, activation='relu')(input)
         h = layers.Dense(128, activation='relu')(h)
-        output = layers.Dense(28*28, activation='sigmoid')(h)
+        output = layers.Dense(28*28, activation='sigmoid')(h)       # generated image has values between 0-1
         model = tf.keras.Model(inputs=input, outputs=output, name='generator')
         return model
 
@@ -24,7 +25,7 @@ class GAN(tf.keras.Model):
         input = layers.Input(shape=(28*28, ))
         h = layers.Dense(256, activation='relu')(input)
         h = layers.Dense(128, activation='relu')(h)
-        output = layers.Dense(1, activation='sigmoid')(h)
+        output = layers.Dense(1, activation='sigmoid')(h)           # P(true image) : 1 - true image, 0 - fake image
         model = tf.keras.Model(inputs=input, outputs=output, name='discriminator')
         return model
 
@@ -32,22 +33,22 @@ class GAN(tf.keras.Model):
         random_noise = tf.random.normal(shape=(batch_size, self.latent_dim))
         return random_noise
 
-    def compile(self,d_optimizer, g_optimizer, loss_fn):
+    def compile(self, d_optimizer, g_optimizer, loss_fn):
         super().compile()
         self.d_optimizer = d_optimizer
         self.g_optimizer = g_optimizer
         self.loss_fn = loss_fn
 
     def train_step(self, real_images):
+        #real_images are array of flattened images.
         if isinstance(real_images, tuple):
             real_images = real_images[0]
         batch_size = tf.shape(real_images)[0]
-        images_flat = tf.reshape(real_images, (-1, 28 * 28))
 
         # Discriminator training
         random_noise = self.sample_random_noise(batch_size)
         fake_images = self.generator(random_noise)
-        combine_images = tf.concat([images_flat, fake_images], axis=0)
+        combine_images = tf.concat([real_images, fake_images], axis=0)
         labels = tf.concat([tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0)
         with tf.GradientTape() as tape:
             predictions = self.discriminator(combine_images)
@@ -60,11 +61,12 @@ class GAN(tf.keras.Model):
 
         # Generator training
         random_noise = self.sample_random_noise(batch_size)
-        labels = tf.zeros((batch_size, 1))
+        # So that we can use the same loss fn of D, we just flip the labels of the data
+        misleading_labels = tf.ones((batch_size, 1))
         with tf.GradientTape() as tape:
             fake_images = self.generator(random_noise)
             predictions = self.discriminator(fake_images)
-            g_loss = self.loss_fn(labels, predictions) * -1     # maximize loss for discriminator
+            g_loss = self.loss_fn(misleading_labels, predictions)
 
         grads = tape.gradient(g_loss, self.generator.trainable_weights)
         self.g_optimizer.apply_gradients(
